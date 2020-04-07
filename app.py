14ff5ee96a1341f0c9b88218628900721429ec86
mugabodeo6@gmail.com <mugabodeo6@gmail.com>
2020-04-07 10:33:18 +0200

    adding south sudan

--- app.py
+++ app.py
@@ -13,14 +13,11 @@ def home():
         "https://pomber.github.io/covid19/timeseries.json").json()
     print(req_data["Rwanda"])
 
-    SSdata = {'date': req_data["Rwanda"][-1]["date"],
-              'confirmed': 0, 'deaths': 0, 'recovered': 0}
-
     return render_template("index.html",
                            Rdata=req_data["Rwanda"][-1],
                            Kdata=req_data["Kenya"][-1],
                            Ugdata=req_data["Uganda"][-1],
                            Tzdata=req_data["Tanzania"][-1],
                            Bdata=req_data["Burundi"][-1],
-                           SSdata=SSdata
+                           SSdata=req_data["South Sudan"][-1]
                            )
